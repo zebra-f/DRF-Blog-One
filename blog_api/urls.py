0688ec7965673fb2e1b52e25a343a1cd4dc73ab0
zebra-f <rafal.zebrowski@gmail.com>
2022-08-26 00:13:31 +0200

    blog_api views PostList PostDetail added. DRF settings. Urls spacing fixed

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-# from .views import PostList, PostDetial
+from .views import PostList, PostDetail
 
 
 app_name = 'blog_api'
 
 urlpatterns = [
-    # path('<int:pk>/', PostDetial.as_view(), name='detailcreate'),
-    # path('', PostList.as_view(), name='listcreate'),
+    path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
+    path('', PostList.as_view(), name='listcreate'),
 ]
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from blog.models import Post
+from .serializers import PostSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+
+
+
+
+
