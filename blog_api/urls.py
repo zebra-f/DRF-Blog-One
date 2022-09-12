be98e713c65e3c73ac5016e017e2d5253353d2d3
zebra-f <rafal.zebrowski@gmail.com>
2022-09-12 23:41:45 +0200

    ReactTest added

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, ReactTest
 
 
 app_name = 'blog_api'
@@ -8,4 +8,5 @@ app_name = 'blog_api'
 urlpatterns = [
     path('<int:pk>/', PostDetail.as_view(), name='detailupdatedelete'),
     path('', PostList.as_view(), name='listcreate'),
+    path('reacttest', ReactTest.as_view(), name='reacttest'),
 ]
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,9 +1,21 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.response import Response
 
 from blog.models import Post
 from .serializers import PostSerializer
 from .permissions import PostUserUpdateDeletePermission
+
+
+class ReactTest(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        react_test = {
+            'test': 'passed, you\'re authenticated'
+        }
+        return Response(react_test)
 
 
 class PostList(generics.ListCreateAPIView):
