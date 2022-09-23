a1bdd4b842328797a650a48f215cfc87a1d8b5fd
zebra-f <rafal.zebrowski@gmail.com>
2022-09-23 23:29:28 +0200

    ViewSets added in place of generic APIViews

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,12 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 
-from .views import PostList, PostDetail, ReactTest
+from rest_framework.routers import DefaultRouter
 
+# from .views import PostList, PostDetail, ReactTest
+from .views import ReactTest, PostViewSet
+
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet,basename="posts")
 
 app_name = 'blog_api'
 
 urlpatterns = [
-    path('<int:pk>/', PostDetail.as_view(), name='detailupdatedelete'),
-    path('', PostList.as_view(), name='listcreate'),
+    # path('<int:pk>/', PostDetail.as_view(), name='detailupdatedelete'),
+    # path('', PostList.as_view(), name='listcreate'),
+    path('', include(router.urls)),
     path('reacttest', ReactTest.as_view(), name='reacttest'),
 ]
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
+
+from time import sleep
 
 from blog.models import Post
 from .serializers import PostSerializer
@@ -15,29 +18,34 @@ class ReactTest(APIView):
         react_test = {
             'test': 'passed, you\'re authenticated'
         }
-        return Response(react_test)
+        return Response(react_test) 
 
 
-class PostList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+# class PostList(generics.ListCreateAPIView):
+#     permission_classes = [IsAuthenticatedOrReadOnly]
+#     queryset = Post.postobjects.all()
+#     serializer_class = PostSerializer
+
+#     def get_queryset(self):
+#         # slowing down this endpoint for a front-end testing 
+#         sleep(1.0)
+#         return super().get_queryset()
+    
+
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#     permission_classes = [PostUserUpdateDeletePermission]
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = [PostUserUpdateDeletePermission]
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
 
-    def get_queryset(self):
-        # slowing down this endpoint for a front-end testing 
-        from time import sleep
+
+    def list(self, request, *args, **kwargs):
         sleep(1.0)
-        return super().get_queryset()
-    
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [PostUserUpdateDeletePermission]
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-
-
-
+        return super().list(request, *args, **kwargs)
 
 
